6401ceab532c7433f343504cdc2cca190da87599
Aevyrie Roessler <aevyrie@gmail.com>
2020-05-20 08:24:03 -0700

    Fixed indentation

--- src/io/dialogs.rs
+++ src/io/dialogs.rs
@@ -4,49 +4,49 @@ use std::path::{Path, PathBuf};
 
 pub async fn open() -> Result<PathBuf, io::Error> {
 
-let result:nfd::Response = match async {
-    return nfd::open_file_dialog(Some("json"), None)
-}.await {
-    Ok(result) => result,
-    Err(e) => return Err(io::Error::new(
-        io::ErrorKind::InvalidData, 
-        "Unable to unwrap data from new file dialog"
-    )),
-};
+    let result:nfd::Response = match async {
+        return nfd::open_file_dialog(Some("json"), None)
+    }.await {
+        Ok(result) => result,
+        Err(e) => return Err(io::Error::new(
+            io::ErrorKind::InvalidData, 
+            "Unable to unwrap data from new file dialog"
+        )),
+    };
 
-let file_string: String = match result {
-    Response::Okay(file_path) => file_path,
-    Response::OkayMultiple(_) => {
+    let file_string: String = match result {
+        Response::Okay(file_path) => file_path,
+        Response::OkayMultiple(_) => {
+            return Err(
+            io::Error::new(
+                io::ErrorKind::InvalidInput, 
+                "Multiple files returned when one was expected"
+            )
+            )
+        }
+        Response::Cancel => {
+            return Err(
+            io::Error::new(
+                io::ErrorKind::Interrupted, 
+                "User cancelled file open"
+            )
+            )
+        }
+    };
+
+    let mut result: PathBuf = PathBuf::new();
+    result.push(Path::new(&file_string));
+
+    if result.exists() {
+        return Ok(result)
+    } else {
         return Err(
         io::Error::new(
-            io::ErrorKind::InvalidInput, 
-            "Multiple files returned when one was expected"
+            io::ErrorKind::NotFound, 
+            "File does not exist"
         )
         )
     }
-    Response::Cancel => {
-        return Err(
-        io::Error::new(
-            io::ErrorKind::Interrupted, 
-            "User cancelled file open"
-        )
-        )
-    }
-};
-
-let mut result: PathBuf = PathBuf::new();
-result.push(Path::new(&file_string));
-
-if result.exists() {
-    return Ok(result)
-} else {
-    return Err(
-    io::Error::new(
-        io::ErrorKind::NotFound, 
-        "File does not exist"
-    )
-    )
-}
 }
 
 pub async fn save() -> Result<PathBuf, io::Error> {
